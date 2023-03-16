ea7ec5afb7ed93bd058f027b29a885208b18a398
DESKTOP-ULURT1J\Xô <vantruong2904078@gmail.com>
2023-03-16 15:37:17 +0700

    Update application

--- backend/src/main/java/mock/project/backend/repository/OrderRepository.java
+++ backend/src/main/java/mock/project/backend/repository/OrderRepository.java
@@ -1,5 +1,10 @@
 package mock.project.backend.repository;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import mock.project.backend.entities.Orders;
+@Repository(value="orderRepo")
+public interface OrderRepository extends JpaRepository<Orders, Integer>{
 
 }
